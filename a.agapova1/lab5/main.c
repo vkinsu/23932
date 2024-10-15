319ebf770d4bab6a756635fece9518b847a9407b
AlexandraAgapova <a.agapova1@g.nsu.ru>
2024-10-15 13:11:26 +0700

    Corrected lab5 with main.c, Makefile and file.txt

--- a.agapova1/lab5/main.c
+++ a.agapova1/lab5/main.c
@@ -2,64 +2,86 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
-#include <sys/types.h>
+#include <ctype.h>
 
-#define MAX_LINES 500
-#define BUFFER_SIZE 257
+#define MAX_LINE_LENGTH 1024
 
-int main(int argc, char *argv[]) {
-	long offsets[MAX_LINES];
-	int fd;
-	int current_line = 1, line_len = 0, input_line;
-	char c, buffer[BUFFER_SIZE];
+int get_line_number() {
+	char buffer[100];
+	int i = 0;
+	int line_number = 0;
 
-	if (argc < 2) {
-		printf("Usage: %s <filename>\n", argv[0]);
-		return 1;
-	}
+	printf("Enter the line number (0 to exit): ");
+	fgets(buffer, sizeof(buffer), stdin);
 
-	if ((fd = open(argv[1], O_RDONLY)) == -1) {
-		perror("Error opening file");
-		return 1;
-	}
-
-	offsets[1] = 0L;
-
-	while (read(fd, &c, 1)) {
-		if (c == '\n') {
-			current_line++;
-			offsets[current_line] = lseek(fd, 0L, SEEK_CUR);
-			line_len = 0;
-		} else {
-			line_len++;
+	while (buffer[i]) {
+		if (isdigit(buffer[i])) {
+			line_number = line_number * 10 + (buffer[i] - '0');
 		}
+		i++;
+	}
+
+	return line_number;
+}
+
+int main() {
+	int fd;
+	off_t offset = 0;
+	int line_number = 0;
+	ssize_t bytes_read;
+	char buffer[MAX_LINE_LENGTH];
+	off_t *offset_table = NULL;
+	int *line_lengths = NULL;
+	int lines_count = 0;
+	char ch;
+
+	fd = open("file.txt", O_RDONLY);
+	if (fd == -1) {
+		perror("Error opening file");
+		exit(1);
+	}
+
+	while ((bytes_read = read(fd, &ch, 1)) > 0) {
+		if (ch == '\n') {
+			lines_count++;
+			offset_table = realloc(offset_table, sizeof(off_t) * lines_count);
+			line_lengths = realloc(line_lengths, sizeof(int) * lines_count);
+			offset_table[lines_count - 1] = offset;
+			line_lengths[lines_count - 1] = lseek(fd, 0L, SEEK_CUR) - offset;
+			offset = lseek(fd, 0L, SEEK_CUR);
+		}
+	}
+
+	printf("Line offset table:\n");
+	for (int i = 0; i < lines_count; i++) {
+		printf("Line %d: offset = %lld, length = %d\n", i + 1, (long long)offset_table[i], line_lengths[i]);
 	}
 
 	while (1) {
-		printf("Enter line number (0 to exit): ");
-		scanf("%d", &input_line);
+		line_number = get_line_number();
 
-		if (input_line == 0) {
+		if (line_number == 0) {
 			break;
 		}
 
-		if (input_line >= current_line || input_line < 1) {
-			printf("Invalid line number\n");
+		if (line_number < 1 || line_number > lines_count) {
+			printf("Invalid line number. Try again.\n");
 			continue;
 		}
 
-		lseek(fd, offsets[input_line], SEEK_SET);
+		lseek(fd, offset_table[line_number - 1], SEEK_SET);
 
-		int bytes_read = 0;
-		int idx = 0;
-		while ((bytes_read = read(fd, &c, 1)) > 0 && c != '\n') {
-			buffer[idx++] = c;
+		bytes_read = read(fd, buffer, line_lengths[line_number - 1]);
+		if (bytes_read > 0) {
+			buffer[bytes_read] = '\0';
+			printf("Line %d: %s\n", line_number, buffer);
 		}
-		buffer[idx] = '\0';
-		printf("Line %d: %s\n", input_line, buffer);
 	}
 
+	free(offset_table);
+	free(line_lengths);
+
 	close(fd);
+
 	return 0;
 }
