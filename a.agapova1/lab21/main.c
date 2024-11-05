08837fa4509b1bc92f25885600066c7f39ca1db6
AlexandraAgapova <a.agapova1@g.nsu.ru>
2024-11-05 13:56:24 +0700

    Added lab21

--- a.agapova1/lab21/main.c
+++ a.agapova1/lab21/main.c
@@ -3,36 +3,21 @@
 #include <signal.h>
 #include <unistd.h>
 
-volatile sig_atomic_t sigint_count = 0;
+int sigint_count = 0;
 
-void handle_sigint(int sig) {
-	sigint_count++;
+void handle_sigint() {
 	printf("\a");
+	fflush(NULL);
+	sigint_count++;
 }
 
-void handle_sigquit(int sig) {
+void handle_sigquit() {
 	printf("\nSIGQUIT signal received. SIGINT signals: %d\n", sigint_count);
 	exit(0);
 }
 
 int main() {
-	struct sigaction sa_int, sa_quit;
-
-	sa_int.sa_handler = handle_sigint;
-	sigemptyset(&sa_int.sa_mask);
-	sa_int.sa_flags = 0;
-
-	sa_quit.sa_handler = handle_sigquit;
-	sigemptyset(&sa_quit.sa_mask);
-	sa_quit.sa_flags = 0;
-
-	sigaction(SIGINT, &sa_int, NULL);
-	sigaction(SIGQUIT, &sa_quit, NULL);
-
-	while (1) {
-		pause();
-	}
-
-	return 0;
+	signal(SIGINT, &handle_sigint);
+	signal(SIGQUIT, &handle_sigquit);
+	while (1);
 }
-
