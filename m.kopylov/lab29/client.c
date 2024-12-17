cfa07529da070bcdc93683e6e788268a2ea49c6d
Matvey Kopylov <kopylovmatveyx@gmail.com>
2024-12-17 14:12:25 +0700

    Lab30 update

--- m.kopylov/lab29/client.c
+++ m.kopylov/lab29/client.c
@@ -8,53 +8,46 @@
 #include <sys/un.h>
 #include <memory.h> 
 
-static char* msgs[] = { "Hello\n", "i am\n", "a Message\n"};
-
 #define BUFF_SIZE 256
-const char* soket_name = "socket";
+#define SOCKET_PATH "socket"
 
 int main(size_t argc, char** argv)
 {
-    printf("Client is starting...\n");
-    char buffer[BUFF_SIZE];
-    int server_sock, client_sock;
-    int answer;
-    int stat;
-    struct sockaddr_un sock_addr, client_addr; 
+        setbuf(stdin, NULL); 
+        printf("Client is starting...\n");
+        char buffer[BUFF_SIZE];
+        int server_sock, client_sock;
+        int answer;
 
-    client_sock = socket(AF_UNIX, SOCK_STREAM, 0);
+        struct sockaddr_un server_addr; 
 
-    if (client_sock == -1) 
-    {
-        perror("Client: Failure with creating socket!\n");
-        exit(EXIT_FAILURE);
-    }
+        client_sock = socket(AF_UNIX, SOCK_STREAM, 0);
 
-    // Cleaning the structure with trash
-    memset((char*)&client_addr, '\0', sizeof(client_addr));
-    client_addr.sun_family = AF_UNIX;
-    strncmp(client_addr.sun_path, soket_name, strlen(soket_name)-1);    
-    unlink(soket_name);  
-
-    if (connect(client_sock, (struct sockaddr*)&client_addr, sizeof(client_addr)) == -1)
-    {
-        perror("Client: Failure with connectint to socket!\n");
-        close(client_sock);
-        exit(EXIT_FAILURE);
-    }
-    printf("Client: Client is started.\n");
-
-    for (int i = 0; i < 3; i++)
-    {
-        if (send(client_sock, msgs[i], strlen(msgs[i]), 0) == -1)
+        if (client_sock == -1) 
         {
-            perror("Client: Error with sending message\n");
+            perror("Client: Failure with creating socket!\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // Cleaning the structure with trash
+        memset((char*)&server_addr, 0, sizeof(struct sockaddr_un));
+        server_addr.sun_family = AF_UNIX;
+        strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path)-1);    
+
+        if (connect(client_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
+        {
+            perror("Client: Failure with connectint to socket!\n");
             close(client_sock);
             exit(EXIT_FAILURE);
         }
-    }
+        printf("Client: Client is started.\n");
+        memset(buffer, 0, BUFF_SIZE);
 
-    shutdown(client_sock, SHUT_RD);
-    close(client_sock);
-    exit(EXIT_SUCCESS);
+        sprintf(buffer, "Message from client pid: %d", getpid());
+            
+        send(client_sock, buffer, strlen(buffer), 0);
+    
+        shutdown(client_sock, SHUT_RD);
+        close(client_sock);
+        exit(EXIT_SUCCESS);
 }
--- m.kopylov/lab29/server.c
+++ m.kopylov/lab29/server.c
@@ -9,12 +9,14 @@
 #include <sys/un.h>
 #include <memory.h> 
 #include <ctype.h>
+#include <poll.h>
 
 #define BUFF_SIZE 256
-const char* soket_name = "socket";
+#define SOCKET_PATH "socket"
 
 void toupper_string(char* buff, int len)
 {
+    int i;
     for (int i = 0; i < len; i++)
     {
         buff[i] = toupper(buff[i]);
@@ -24,94 +26,148 @@ void toupper_string(char* buff, int len)
 int main(size_t argc, char** argv)
 {
     char buffer[BUFF_SIZE];
+    setbuf(stdout, NULL); 
     pid_t proc = fork();
-
-    switch (proc)
+    
+    if (proc == 0)
     {
-    case 0:
+        if (access(SOCKET_PATH, F_OK) != 0) 
         {
-            printf("Server: Starting client from child process...\n");
-
-            char* argv[] = {"./client", NULL};
-            int val = execvp("./client", argv);
-            if (val == -1)
-            {
-                perror("Server: Something went wrong in the child process!\n");
-                exit(EXIT_FAILURE);
-            }
-            break;
-        }
-    case -1: 
+            perror("Server: Socket file not ready\n");
+            sleep(1);
+        }  
+        printf("Server: Starting client from child process...\n");
+        char* argv[] = {"./client", NULL};
+        int val = execvp("./client", argv);
+        if (val == -1)
         {
-            printf("Server: Something went wrong!\n");
+            perror("Server: Something went wrong in the child process!\n");
             exit(EXIT_FAILURE);
-            break;
-        }
-    default:
-        {
-            printf("Server: Server is starting...\n");
-            
-
-            int server_sock, client_sock;
-            int answer;
-            int stat;
-            struct sockaddr_un sock_addr, client_addr; 
-
-            server_sock = socket(AF_UNIX, SOCK_STREAM, 0);
-
-            if (server_sock == -1) 
-            {
-                perror("Server: Failure with creating socket!\n");
-                exit(EXIT_FAILURE);
-            }
-
-            // Cleaning the structure with trash
-            memset((char*)&sock_addr, '\0', sizeof(sock_addr));
-            sock_addr.sun_family = AF_UNIX;
-            strncmp(sock_addr.sun_path, soket_name, strlen(soket_name)-1);      
-            unlink(soket_name);  
-
-            if (bind(server_sock, (struct sockaddr *)&sock_addr, sizeof(sock_addr)) == -1)
-            {
-                perror("Server: Failure with bind socket!\n");
-                close(server_sock);
-                exit(EXIT_FAILURE);
-            }
-          
-            if (listen(server_sock, 1) == -1)
-            {
-                perror("Server: Failure with listening socket!\n");
-                close(server_sock);
-                exit(EXIT_FAILURE);
-            }
-           
-            printf("Server: Server is started.\n");
-            int len = 0;
-            int msgs = 1;
-            while (1)
-            {
-                answer = sizeof(client_addr);
-                client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &answer);
-                answer = recv(client_sock, buffer, BUFF_SIZE, 0); 
-                if (answer <= 0)
-                {
-                    printf("Server: Server is stopping\n");
-                    
-                    break;
-                }
-
-                len = strlen(buffer);
-
-                toupper_string(buffer, len);
-                printf("%s", buffer);
-                
-              
-                shutdown(server_sock, SHUT_RD);
-                close(server_sock);
-                break;
-            }
         }
     }
+    else if (proc == -1)
+    {
+        printf("Server: Something went wrong!\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        printf("Server: Server is starting...\n");
 
-    exit(EXIT_SUCCESS);
+        int server_sock, client_sock;
+        int answer, len_client;
+        struct sockaddr_un server_addr, client_addr; 
+
+        len_client = sizeof(client_addr);
+        server_sock = socket(AF_UNIX, SOCK_STREAM, 0);
+
+        if (server_sock == -1) 
+        {
+            perror("Server: Failure with creating socket!\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // Cleaning the structure with trash
+        memset((char*)&server_addr, '\0', sizeof(server_addr));
+        server_addr.sun_family = AF_UNIX;
+        strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path)-1);  
+        unlink(SOCKET_PATH);  
+
+        if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+        {
+            perror("Server: Failure with bind socket!\n");
+            close(server_sock);
+            exit(EXIT_FAILURE);
+        }
+
+        chmod(SOCKET_PATH, 0777);
+        if (listen(server_sock, 1) == -1)
+        {
+            perror("Server: Failure with listening socket!\n");
+            close(server_sock);
+            exit(EXIT_FAILURE);
+        } 
+                    
+        printf("Server: Server is started.\n");
+
+        struct pollfd clients[10];
+        int client_count = 1;
+
+        clients[0].fd = server_sock;    
+        clients[0].events = POLLIN;    
+
+        for (int i = 1; i < 10; i++)
+        {
+            clients[i].fd = -1;
+        }
+
+        while (1)
+        {
+            // Listen activities 
+            int activity = poll(clients, client_count, -1);  // Блокируемся до события
+            if (activity < 0) 
+            {
+                perror("Server: poll error\n");
+                close(server_sock);
+                exit(EXIT_FAILURE);
+            }
+                        
+            // New client
+            if (clients[0].revents & POLLIN)
+            {
+                client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &len_client);
+                if (client_sock < 0)
+                {
+                    perror("Server: accept error\n");
+                    close(server_sock);
+                    exit(EXIT_FAILURE);
+                }
+
+                for (int i = 1; i < 10; i++) {
+                if (clients[i].fd == -1) {
+                    clients[i].fd = client_sock;
+                    clients[i].events = POLLIN;
+                    if (i >= client_count) client_count = i + 1;
+                    break;
+                }
+            }
+
+                if (client_count == 10)
+                {
+                    perror("Server: accept error (too many clients)\n");
+                    close(server_sock);
+                    exit(EXIT_FAILURE);
+                }
+            }
+                            
+            for (int i = 1; i < client_count; i++)
+            {
+                if (clients[i].fd == -1) continue;
+
+                if (clients[i].revents & POLLIN)
+                {
+                    memset(buffer,0, BUFF_SIZE);
+                    answer = recv(clients[i].fd, buffer, BUFF_SIZE, 0);
+
+                    if (answer <= 0)
+                    {
+                        printf("Client %d is closed\n", clients[i].fd);
+                        close(clients[i].fd);
+                        clients[i].fd = -1;  // Удаляем из массива      
+                    }
+                    else
+                    {
+                        buffer[answer] = '\0';
+                        toupper_string(buffer, strlen(buffer));
+                        printf("%s\n", buffer);
+                    }
+                }
+            }
+        } 
+        
+        shutdown(server_sock, SHUT_RD);
+        close(server_sock);
+        exit(EXIT_SUCCESS);   
+    }
+    exit(EXIT_SUCCESS);   
 }
